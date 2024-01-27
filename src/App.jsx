c7d8a19f3a48d3801ba849f980bea366663742ec
Bharat Nagda <109472466+Bharatnagda@users.noreply.github.com>
2024-01-27 18:04:26 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./Component/Header/Header"
 import Footer from "./Component/Footer/Footer"
+import Home from "./Component/Home/Home"
 function App() {
   return (
     <>
       <Header/>
+      <Home/>
       <Footer/>
     </>
   )
